261838bdc1ab571a9b4ba64cd88822596136bdbd
jesira <jesiraruhi@yahoo.com>
2021-11-09 08:44:47 +0600

    store and cart

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,10 +17,18 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 from django.conf.urls.static import static
-from django.conf import settings
+#from django.conf import settings
+from django.contrib.staticfiles.views import serve
+from django.views.decorators.cache import never_cache
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+
+    urlpatterns += static(settings.STATIC_URL, view=never_cache(serve))
--- store/models.py
+++ store/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from category.models import Category
 from django.urls import reverse
+from accounts.models import Account
+from django.db.models import Avg, Count
+
 # Create your models here.
 
 class Product(models.Model):
@@ -16,7 +19,45 @@ class Product(models.Model):
     modified_date   = models.DateTimeField(auto_now=True)
 
     def get_url(self):
-        return reverse('product_detail', args=[self.category.slug, self.slug ])
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
+
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value     = models.CharField(max_length=100)
+    is_active           = models.BooleanField(default=True)
+    created_date        = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
